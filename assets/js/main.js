4a7681fe3145ed353ee848dc6d0ab312a29dec2c
yejooSeo1119 <108320583+yejooSeo1119@users.noreply.github.com>
2024-11-20 12:07:20 +0900

    1120 모바일

--- assets/js/main.js
+++ assets/js/main.js
@@ -39,39 +39,39 @@ Author: GrayGrids
         }
     };
 
-    // section menu active
-	function onScroll(event) {
-		var sections = document.querySelectorAll('.page-scroll');
-		var scrollPos = window.pageYOffset || document.documentElement.scrollTop || document.body.scrollTop;
+    // // section menu active
+	// function onScroll(event) {
+	// 	var sections = document.querySelectorAll('.page-scroll');
+	// 	var scrollPos = window.pageYOffset || document.documentElement.scrollTop || document.body.scrollTop;
 
-		for (var i = 0; i < sections.length; i++) {
-			var currLink = sections[i];
-			var val = currLink.getAttribute('href');
-			var refElement = document.querySelector(val);
-			// var scrollTopMinus = scrollPos + 73;
-			if (refElement.offsetTop <= scrollTopMinus && (refElement.offsetTop + refElement.offsetHeight > scrollTopMinus)) {
-				document.querySelector('.page-scroll').classList.remove('active');
-				currLink.classList.add('active');
-			} else {
-				currLink.classList.remove('active');
-			}
-		}
-	};
+	// 	for (var i = 0; i < sections.length; i++) {
+	// 		var currLink = sections[i];
+	// 		var val = currLink.getAttribute('href');
+	// 		var refElement = document.querySelector(val);
+	// 		// var scrollTopMinus = scrollPos + 73;
+	// 		if (refElement.offsetTop <= scrollTopMinus && (refElement.offsetTop + refElement.offsetHeight > scrollTopMinus)) {
+	// 			document.querySelector('.page-scroll').classList.remove('active');
+	// 			currLink.classList.add('active');
+	// 		} else {
+	// 			currLink.classList.remove('active');
+	// 		}
+	// 	}
+	// };
 
     window.document.addEventListener('scroll', onScroll);
     
-    // // for menu scroll 
-    // var pageLink = document.querySelectorAll('.page-scroll');
+    // for menu scroll 
+    var pageLink = document.querySelectorAll('.page-scroll');
 
-    // pageLink.forEach(elem => {
-    //     elem.addEventListener('click', e => {
-    //         e.preventDefault();
-    //         document.querySelector(elem.getAttribute('href')).scrollIntoView({
-    //             behavior: 'smooth',
-    //             offsetTop: 1 - 60,
-    //         });
-    //     });
-    // });
+    pageLink.forEach(elem => {
+        elem.addEventListener('click', e => {
+            e.preventDefault();
+            document.querySelector(elem.getAttribute('href')).scrollIntoView({
+                behavior: 'smooth',
+                offsetTop: 1 - 60,
+            });
+        });
+    });
 
     //===== close navbar-collapse when a  clicked
     let navbarToggler = document.querySelector(".navbar-toggler");
